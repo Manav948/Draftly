9a6f9817c3947fad88000ab7e76cc14144d087b4
Manav948 <valanimanav61@gmail.com>
2025-10-11 23:30:04 +0530

    fix issue

--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -20,6 +20,7 @@ const Sidebar = () => {
       <aside
         className={`
           fixed lg:static top-0 left-0 h-screen z-50 flex
+          
           bg-white dark:bg-gray-950 border-r border-gray-300 dark:border-gray-700
           transition-transform duration-300 ease-in-out
           ${isOpen ? "translate-x-0 shadow-lg" : "-translate-x-full lg:translate-x-0"}
